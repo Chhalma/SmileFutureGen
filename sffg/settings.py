fac299cf8fd98c7da63cc25e6d451742badc5d14
Chhalma Chhaya <chhaya.mist@gmail.com>
2024-11-15 23:47:02 +0000

    Updated settings.py to load .env

--- sffg/settings.py
+++ sffg/settings.py
@@ -20,7 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$l&!q&x*a#vh&!)o03m21xxygth)dtr5kwb5xay%!s)fp&-!^0"
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+# Use the SECRET_KEY from the .env file
+DJANGO_SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
