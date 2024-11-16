7962795cdf392035a2ecd80c0f6f91abd424b5a6
Chhalma Chhaya <chhaya.mist@gmail.com>
2024-11-16 22:48:25 +0000

    changes in .env and settings.py

--- sffg/settings.py
+++ sffg/settings.py
@@ -20,18 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from decouple import Config, Csv
 import os
-from dotenv import load_dotenv
-# Load environment variables from .env file
-load_dotenv()
 
-# Use the SECRET_KEY from the .env file
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+# Load the environment variables from the .env file
+config = Config()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# Set the Django settings from the environment variables
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
